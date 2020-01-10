a506a2667a844fcc1f69e19c571e5d5297455ead
Shaw007134 <gggshaw007134@gmail.com>
2020-01-10 17:05:17 +0800

    首页添加响应式 菜单显示与隐藏

--- js/hcharts_config.js
+++ js/hcharts_config.js
@@ -1,100 +1,101 @@
-function hchartsConfig(){
+function hchartsConfig() {
   var chart = {
     zoomType: 'xy'
   };
   var title = {
-      text: '合肥24小时实时气象数据'   
+    text: '合肥24小时实时气象数据'
   };
   var xAxis = {
-      categories: [],
-      crosshair: true
-  }; 
-  var yAxis= [{ // 第一条Y轴
+    categories: [],
+    crosshair: true
+  };
+  var yAxis = [{ // 第一条Y轴
       title: {
         text: '降雨量',
         style: {
-            color: Highcharts.getOptions().colors[0]
+          color: Highcharts.getOptions().colors[0]
         }
       },
       labels: {
         format: '{value} mm',
         style: {
-            color: Highcharts.getOptions().colors[0]
+          color: Highcharts.getOptions().colors[0]
         }
       },
-      opposite:true      
-  }, { // 第二条Y轴
+      opposite: true
+    }, { // 第二条Y轴
       gridLineWidth: 0,
       title: {
         text: '气压',
         style: {
-            color: Highcharts.getOptions().colors[2]
+          color: Highcharts.getOptions().colors[2]
         }
       },
       labels: {
         format: '{value} mb',
         style: {
-            color: Highcharts.getOptions().colors[2]
+          color: Highcharts.getOptions().colors[2]
         }
       },
-  }, 
-  { // 第三条Y轴
-    labels: {
-      format: '{value}\xB0C',
-      style: {
+    },
+    { // 第三条Y轴
+      title: {
+        text: '气温',
+        style: {
           color: Highcharts.getOptions().colors[3]
-      }
-    },
-    title: {
-      text: '气温',
-      style: {
+        }
+      },
+      labels: {
+        format: '{value}\xB0C',
+        style: {
           color: Highcharts.getOptions().colors[3]
+        }
       }
-    },
-  }, { // 第四条Y轴
-    gridLineWidth: 0,
-    title: {
-      text: '相对湿度',
-      style: {
+    }, { // 第四条Y轴
+      gridLineWidth: 0,
+      title: {
+        text: '相对湿度',
+        style: {
           color: Highcharts.getOptions().colors[4]
-      }
-    },
-    labels: {
-      format: '{value} %',
-      style: {
+        }
+      },
+      labels: {
+        format: '{value} %',
+        style: {
           color: Highcharts.getOptions().colors[4]
-      }
-    },
-    opposite:true  
-}];
+        }
+      },
+      opposite: true
+    }
+  ];
 
-  var series= [{
-    name: '降雨量',
-       type: 'column',
-       yAxis: 1,
-       data: [],
-       tooltip: {
-           valueSuffix: ' mm'
-       }
+  var series = [{
+      name: '降雨量',
+      type: 'column',
+      yAxis: 0,
+      data: [],
+      tooltip: {
+        valueSuffix: ' mm'
+      }
 
     },
     {
       name: '气压',
       type: 'spline',
-      yAxis: 2,
+      yAxis: 1,
       data: [],
       // color: "#008800",
       tooltip: {
-          valueSuffix: ' hPa'
+        valueSuffix: ' hPa'
       }
     },
     {
       name: '气温',
       type: 'spline',
-      yAxis: 0,
+      yAxis: 2,
       data: [],
       tooltip: {
-          valueSuffix: '\xB0C'
+        valueSuffix: '\xB0C'
       }
     },
     {
@@ -103,25 +104,26 @@ function hchartsConfig(){
       yAxis: 3,
       data: [],
       tooltip: {
-          valueSuffix: ' %'
+        valueSuffix: ' %'
       }
-    }];
+    }
+  ];
   var colors = [];
   var tooltip = {
-      shared: true
+    shared: true
   };
   var credits = {
-      enabled:false
+    enabled: false
   };
   var legend = {
-      layout: 'horizontal',
-      // align: 'left',
-      // x: 0,
-      verticalAlign: 'top',
-      y: 1,
-      // floating: true,
-      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-  };  
+    layout: 'horizontal',
+    // align: 'left',
+    // x: 0,
+    verticalAlign: 'top',
+    y: 1,
+    // floating: true,
+    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+  };
   var colors = ["#7cb5ec", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
   var json = {};
   json.chart = chart;
@@ -135,4 +137,3 @@ function hchartsConfig(){
   json.colors = colors;
   return json;
 }
-
--- js/index.js
+++ js/index.js
@@ -1,12 +1,48 @@
-layui.use(['carousel','element'], function(){
+layui.use(['carousel', 'element'], function () {
   var carousel = layui.carousel;
-  //轮播实例
   var element = layui.element;
+  var $ = layui.jquery;
+
+  //轮播实例
   carousel.render({
     elem: '#banner',
     width: '100%',
     height: '649px',
     arrow: 'always',
+  });
+
+  //滚动监听
+  $(window).scroll(function () {
+    var scr = $(document).scrollTop();
+    scr > 0 ? $(".nav").addClass('scroll') : $(".nav").removeClass('scroll');
+  });
+
+  //导航切换
+  var btn = $('.nav').find('.nav-list').children('button'),
+    spa = btn.children('span'),
+    ul = $('.nav').find('.nav-list').children('.layui-nav');
+
+  btn.on('click', function () {
+    if (!$(spa[0]).hasClass('spa1')) {
+      spa[0].className = 'spa1';
+      spa[1].style.display = 'none';
+      spa[2].className = 'spa3';
+      ul.css('display', 'block');
+      ul.addClass('lay-nav-coll')
+      ul.children('.layui-nav-item').addClass('lay-nav-coll-item')
+      // ul.children('.layui-nav-item').css('line-height', '40px')
+      $('.nav')[0].style.height = 90 + ul[0].offsetHeight + 'px';
+      console.log($('.nav')[0].style.height)
+    } else {
+      spa[0].className = '';
+      spa[1].style.display = 'block';
+      spa[2].className = '';
+      ul.removeClass('lay-nav-coll');
+      // ul.addClass('lay-nav-hide');
+      ul.children('.layui-nav-item').removeClass('lay-nav-coll-item')
+      ul.css('display', 'none');
+      $('.nav')[0].style.height = 80 + 'px';
+    }
   });
 });
 
